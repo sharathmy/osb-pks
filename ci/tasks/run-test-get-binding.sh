517c828a4828601b15f99705f5ff535d0ee5c748
nouseforaname <kkiess@starkandwayne.com>
2018-09-13 15:21:46 +0200

    fixed get binding script

--- ci/tasks/run-test-get-binding.sh
+++ ci/tasks/run-test-get-binding.sh
@@ -18,7 +18,7 @@ PCF_API_CERT=$(echo | openssl s_client -connect $PCF_API_FQDN:443 -showcerts | o
 PCF_UAA_CERT=$(echo | openssl s_client -connect $PCF_UAA_FQDN:443 -showcerts | openssl x509)
 
 import_self_signed_certs "$PKS_API_CERT" "$PKS_UAA_CERT" "$PCF_API_CERT" "$PCF_UAA_CERT" "$PCF_CA_CERT"
-
+SB=admin:pass@localhost:8080
 nohup java -jar osb-pks-pre-release/osb_pks.jar &
 wait_for_osb
 
@@ -34,7 +34,7 @@ echo $SI_CREDENTIALS | jq .
 bosh int <(echo $SI_CREDENTIALS | jq .credentials.k8s_context) > kube-config
 #RUN TEST WITH KUBECTL
 kubectl --kubeconfig ./kube-config get namespaces
-cp test-cluter-data/keyval.properties updated-test-cluster-data/keyval.properties
-
+#UPDATE TEST-CLUSTER-DATA
+cp test-cluster-data/keyval.properties updated-test-cluster-data/keyval.properties
 echo 'SI_BINDING_ID="'"$SI_BINDING_ID"'"' >> updated-test-cluster-data/keyval.properties
 echo 'SI_CREDENTIALS="'"$SI_CREDENTIALS"'"' >> updated-test-cluster-data/keyval.properties
